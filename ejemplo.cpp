324f831f57a0b234a062ebcdf7bd705b8ed1b977
vparedes15 <vaparedes15@gmail.com>
2021-02-26 12:41:07 -0500

    escrbiendo variable a de la linea de comandos

--- ejemplo.cpp
+++ ejemplo.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include <cstdlib>
 
 int main (int arg, char **argv)
 {
+  int a= std::atoi(argv[1]);
+  a= 2*a;
+  
   std::cout << "Hola mundo\n";
+  std::cout <<"a: " <<a<< std::endl; 
 
 
   return 0;
